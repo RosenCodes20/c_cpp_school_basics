43cc4e560c9ab06f1355ff41058790d0c446357e
Rosen Ivanov <rrirrirri08@gmail.com>
2026-02-12 20:00:24 +0200

    feat: made some things at the start of singleton

--- week17/zadacha_2_singleton.cpp
+++ week17/zadacha_2_singleton.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
 #include <string>
+#include <map>
 
 using namespace std;
 
+class Configuration {
+private:
+    map<string, string> settings;
+    Configuration() {}
+    Configuration(const Configuration&) = delete;
+    Configuration& operator = (const Configuration&) = delete;
+
+public:
+    void setSettings(string key, string value) {
+        this->settings[key] = value;
+    }
+
+    string getSetting(string key) {
+        return this->settings[key];
+    }
+};
