eee68a9a802b27b9b839a92f2d304a672fa774f6
Rosen Ivanov <rrirrirri08@gmail.com>
2026-02-12 20:49:50 +0200

    feat: finished the factory

--- week18/zadacha_1_factory.cpp
+++ week18/zadacha_1_factory.cpp
@@ -11,18 +11,21 @@ public:
 };
 
 class Dog : public Animal {
+public:
     void speak() override {
         cout << "Dog says WOOF!" << endl;
     };
 };
 
 class Cat : public Animal {
+public:
     void speak() override {
         cout << "Cat says: MEOW!" << endl;
     }
 };
 
 class AnimalFactory {
+public:
     static std::unique_ptr<Animal> create(const std::string& name) {
         if (name == "Dog") {
             return make_unique<Dog>();
@@ -33,3 +36,13 @@ class AnimalFactory {
         return nullptr;
     }
 };
+
+int main() {
+    AnimalFactory animal_factory;
+
+    std::unique_ptr<Animal> dog = animal_factory.create("Dog");
+    unique_ptr<Animal> cat = animal_factory.create("Cat");
+
+    dog->speak();
+    cat->speak();
+}
